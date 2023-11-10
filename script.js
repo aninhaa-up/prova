4648433a977ba1212da2e77742a9752ee8f786bf
aninhaa-up <134522444+aninhaa-up@users.noreply.github.com>
2023-11-10 12:32:05 -0300

    alert

--- script.js
+++ script.js
@@ -6,4 +6,6 @@ function mudarTexto() {
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "SO COMPRAR UM SKATE!";
 }
-    
+    <button onclick="exibirAlerta()">Clique para exibir o alerta</button> {
+        <script src="script.js"></script>  
+    }
